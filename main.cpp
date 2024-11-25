b55b46533044d37ad0f355d71661327c93f88e6e
ryuto-alt <k023g0078@g.neec.ac.jp>
2024-11-25 16:51:43 +0900

    ge_4-2完成

--- main.cpp
+++ main.cpp
@@ -5,52 +5,51 @@
 
 // ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-    if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
-        return true;
-    }
-    switch (msg) {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
-    return DefWindowProc(hwnd, msg, wparam, lparam);
+	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
+		return true;
+	}
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-    HRESULT hr = CoInitializeEx(0, COINIT_MULTITHREADED);
-    assert(SUCCEEDED(hr));
+	HRESULT hr = CoInitializeEx(0, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
 
-    // ウィンドウアプリケーションの初期化
-    WinApp* winApp = new WinApp;
-    winApp->Initialize();
+	// ウィンドウアプリケーションの初期化
+	WinApp* winApp = new WinApp;
+	winApp->Initialize();
 
-    // DirectX基盤部分の初期化を行うクラス
-    DirectXCommon* dxCommon = new DirectXCommon;
-    dxCommon->Initialize(winApp);
+	// DirectX基盤部分の初期化を行うクラス
+	DirectXCommon* dxCommon = new DirectXCommon;
+	dxCommon->Initialize(winApp);
 
-    // メインループ
-    MSG msg = {};
-    while (msg.message != WM_QUIT) {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        else {
-            dxCommon->Begin(); // 描画開始
-           
-            // - シーンの更新
-            // - オブジェクトの描画
-            // - 入力の処理
-            // - UIの描画
+	// メインループ
+	MSG msg = {};
+	while (true) {
+		if (winApp->ProcessMessage()) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			break;
+		}
+		dxCommon->Begin(); // 描画開始
 
-            dxCommon->End();   // 描画終了
-        }
-    }
+		// - シーンの更新
+		// - オブジェクトの描画
+		// - 入力の処理
+		// - UIの描画
 
-    // リソースの解放
-    delete dxCommon;
-    delete winApp;
-    CoUninitialize();
+		dxCommon->End();   // 描画終了
+	}
 
-    return 0;
+	// リソースの解放
+	delete dxCommon;
+	delete winApp;
+	CoUninitialize();
+
+	return 0;
 }
